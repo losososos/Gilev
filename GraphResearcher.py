71c11b9fb70d6f15c7d1d0a0924ecb2584b5e5c8
wha <112784018+losososos@users.noreply.github.com>
2023-01-11 23:43:52 +0300

    Update GraphResearcher.py

--- GraphResearcher.py
+++ GraphResearcher.py
@@ -1,16 +1,20 @@
-def symm(inputArr, n):
-    fl = 0
+def symm(inputArr):
+    vcount = 0
+    count = 0
+    for i in (inputArr):
+        if i[0] != i[1]:
+            count += 1
     for i in range(len(inputArr)):
         pair = list(reversed(inputArr[i]))
         for j in range(len(inputArr)):
-            if pair == inputArr[j] and i != j:
-                fl += 1
-    if float(fl) == float(n) / 2:
+            if pair[0] != pair[1] and pair == inputArr[j] and i != j:
+                vcount += 1
+    if vcount == count:
         print("Симметричный")
-    elif fl == 0:
-        print("Несимметричный")
-    else:
+    elif vcount == 0:
         print("Антисимметричный")
+    else:
+        print("Несимметричный")
 
 
 def refl(inputArr, n):
@@ -22,9 +26,9 @@ def refl(inputArr, n):
             fl = True
             count += 1
     if (n != count) and fl:
-        print("Антирефлексивный")
-    elif not fl:
         print("Нерефлексивный")
+    elif not fl:
+        print("Антирефлексивный")
     elif fl and (n == count):
         print("Рефлексивный")
 
@@ -37,24 +41,23 @@ def trnz(inputArr):
         for j in range(len(inputArr)):
             pairi = inputArr[i]
             pairj = inputArr[j]
-            if pairj[0] != pairj[1] and pairi[0] != pairi[1] and pairi != pairj:
-                if pairi[1] == pairj[0] and pairi[0] != pairj[1]:
-                    ppair = [pairj[1], pairi[0]]
-                    if ppair in inputArr:
-                        fl = True
-                        elemArr.append(pairi[0])
-                        elemArr.append(pairi[1])
-                        elemArr.append(pairj[1])
-                    else:
-                        flnot = True
-                elif pairi[0] not in elemArr and pairi[1] not in elemArr and pairj[1] not in elemArr:
+            if pairj[0] != pairj[1] and pairi[0] != pairi[1] and pairi != pairj and pairi[1] == pairj[0] and pairi[0] != pairj[1]:
+                ppair = [pairi[0], pairj[1]]
+                if ppair in inputArr:
+                    fl = True
+                    elemArr.append(pairi[0])
+                    elemArr.append(pairi[1])
+                    elemArr.append(pairj[1])
+                else:
+                    flnot = True
+                if pairi[0] not in elemArr and pairi[1] not in elemArr and pairj[1] not in elemArr:
                     flnot = True
     if fl and flnot:
-        print("Антитранзитивный")
+        print("Нетранзитивный")
     elif fl and not (flnot):
         print("Транзитивный")
     else:
-        print("Нетранзитивный")
+        print("Антитранзитивный")
 
 
 strin = input()
@@ -64,6 +67,6 @@ for i in range(infoArr[0]):
     strin = input()
     inputArr.append([int(x) for x in strin.split()])
 
-symm(inputArr, infoArr[0])
+symm(inputArr)
 refl(inputArr, infoArr[1])
 trnz(inputArr)
